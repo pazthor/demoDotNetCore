211000d4f62c6c62e48481a44b449cabd68b72a7
Julio César Pastor Marcelo <final.africae@gmail.com>
2019-04-04 13:58:43 -0600

    Add controller  and view for Student.

---
+++ demoDotNetCore/demoDotNetCore/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using demoDotNetCore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace demoDotNetCore.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
