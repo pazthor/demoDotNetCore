9fe139276f2dd8de13a22d540432dd2d474c7061
Julio César Pastor Marcelo <final.africae@gmail.com>
2019-04-04 13:48:56 -0600

    add the db initializer and conf for communication for the database.

---
+++ demoDotNetCore/demoDotNetCore/Data/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using demoDotNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace demoDotNetCore.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        // se renombram las tablas porque :
+        // Property names for collections are typically plural(Students rather 
+        // than Student), but developers disagree about whether table names 
+        // should be pluralized or not.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
